05a60bb4e2f7bcca000e0fd242d3b8b8daebbaa5
rishabh537 <rishabh537>
2021-09-16 13:25:35 +0530

    pagination and checbox for radio stream with api call

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,21 +1,22 @@
 export const tableStyle = {
-    container:{
-        padding:20
+  container: {
+    backgroundColor: "white",
+    padding: "2% 2.5%",
+  },
+  head: {
+    color: "rgb(156, 164, 171)",
+    fontWeight: 'bold',
+    fontSize: 10,
+  },
+  body: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: "#343F5B",
+  },
+  tableRow: {
+    "&:hover": {
+      boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 1px 5px rgba(0,0,0,0.22)",
+      cursor: "pointer",
     },
-    head: {
-        color: "rgb(156, 164, 171)",
-        fontWeight:'bold',
-        fontSize:10,
-      },
-      body: {
-        fontSize: 10,
-        fontWeight:'bold',
-        color: "rgb(52, 63, 132)",
-      },
-      tableRow: {
-        "&:hover": {
-          boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 1px 5px rgba(0,0,0,0.22)",
-          cursor: "pointer",
-        },
-      },
+  },
 }
--- src/pages/Monitor/Components/Hits.js
+++ src/pages/Monitor/Components/Hits.js
@@ -1,90 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import { connect } from 'react-redux';
 // import * as actionCreators from '../store/Actions/index';
-import Tooltip from '@material-ui/core/Tooltip';
-import InfoIcon from '@material-ui/icons/Info';
-import ButtonSpinner from './ButtonSpinner';
+import Tooltip from "@material-ui/core/Tooltip";
+import InfoIcon from "@material-ui/icons/Info";
+import ButtonSpinner from "./ButtonSpinner";
 // import { getAccessToken, getUserId } from '../apiManager/AuthHelper';
 // import apiUrl from '../apiManager/apiUrl';
-import { getAccessToken, getUserId } from '../../../services/https/AuthHelper';
-import httpUrl from '../../../services/https/httpUrl';
-import axios from 'axios';
-import { monthRange, todayRange, unauthorizedRedirection, weekRange, yearRange } from '../../../utils/HelperMethods';
-import { log } from '../../../utils/app.debug';
+import { getAccessToken, getUserId } from "../../../services/https/AuthHelper";
+import httpUrl from "../../../services/https/httpUrl";
+import axios from "axios";
+import {
+  monthRange,
+  todayRange,
+  unauthorizedRedirection,
+  weekRange,
+  yearRange,
+} from "../../../utils/HelperMethods";
+import { log } from "../../../utils/app.debug";
 import { useDispatch, useSelector } from "react-redux";
-import Communication from '../../../services/https/Communication';
+import Communication from "../../../services/https/Communication";
 
 function Hits(props) {
-    const dispatch = useDispatch();
-    const { session } = useSelector((state) => ({
-        session: state.session,
-      }));
-    const {radioId} = props;
-    const [count, setCount] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-     log("Session",session);
-    useEffect(() => {
-        setLoading(true);
-        const ownerId = getUserId();
-        console.log(ownerId);
-        // const monthRang = monthRange().split(',');
-        const yearRang = yearRange().split(',');
-        Communication.fetchRadioStationHits(radioId)
-        .then((data) => {
-          console.log("Count For Radio Station", data);
-          
-        }).catch(error =>{
-            console.log('Hit Error',error);
-        })
-        // axios({
-        //         method: 'get',
-        //         url: `${httpUrl.API_URL}/detections/owners/${ownerId}/RADIOSTATION/count?radioStation=${radioId}`,
-        //         headers: {
-        //             "Authorization":`Bearer ${getAccessToken()}`
-        //         }
-        //   })
-        //   .then(res => {
-        //         setLoading(false);
-        //         setError(false);
-        //         setCount(res.data);
-        //         // log("radiohits response", res);
-        //    })
-        //    .catch(err=>{
-        //             setLoading(false);
-        //             log("fetch Radiostation table SonicKey Count/hits column error",err.response);
-        //             if (err.response) {
-        //                 unauthorizedRedirection(err.response.status);
-        //                 setError(err.response.data.message);
-        //             } else if (err.request) {
-        //                 setError("Request error for fetch radiostations!!!");                  
-        //             } else {
-        //                 setError("Unexpected error occured while fetching radiostations!!!");       
-        //             }
-        //         }
-        //    );
-    },[]);
-    return (
-        <div style={styles.mainContainer}>
-            {loading ? <ButtonSpinner /> : error ? 
-            <Tooltip title={error}>
-            <p style={{fontSize: 10, color : 'red', cursor: 'pointer'}}>Error
-                <InfoIcon style={{ color: 'red', marginRight: 4, fontSize: 12 }} />
-            </p>
-            </Tooltip>
-            :count }
-        </div>
-    )
+  
+  const { radioId } = props;
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    setLoading(true);
+    Communication.fetchRadioStationHits(radioId)
+      .then((res) => {
+        setLoading(false);
+        setError(false);
+        setCount(res);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div style={styles.mainContainer}>
+      {loading ? (
+        <ButtonSpinner />
+      ) : error ? (
+        <Tooltip title={error}>
+          <p style={{ fontSize: 10, color: "red", cursor: "pointer" }}>
+            Error
+            <InfoIcon style={{ color: "red", marginRight: 4, fontSize: 12 }} />
+          </p>
+        </Tooltip>
+      ) : (
+        count
+      )}
+    </div>
+  );
 }
-
 
 export default Hits;
 
 const styles = {
-    mainContainer : {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        flex: 1,
-    },
+  mainContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
 };

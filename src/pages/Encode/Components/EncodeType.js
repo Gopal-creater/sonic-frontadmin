3cb4726d13858c7357b678770cb57e85de77f3aa
krishnaArba <krishna@arbadev.com>
2022-02-21 13:42:22 +0530

    Encode input fields, texts changed and button changed

---
+++ src/pages/Encode/Components/EncodeType.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { FormControl, MenuItem } from "@material-ui/core";
+import { StyledSelect, StyledSelectInput } from "../../../StyledComponents/StyledAppTextInput/StyledAppSelectInput";
+import { makeStyles } from "@material-ui/styles";
+import theme from "../../../theme";
+
+const useStyles = makeStyles({
+    root: {
+        "&:hover .MuiInput-input": {
+            color: `${theme.colors.primary.navy}`
+        },
+    },
+    select: {
+        borderRadius: 0,
+        boxShadow: 'none',
+        border: `2px solid ${theme.colors.secondary.lightNavy}`,
+        backgroundColor: '#fff',
+        marginTop: -2,
+
+        "& li": {
+            fontSize: `${theme.fontSize.h5}`,
+            color: `${theme.colors.secondary.grey}`,
+            borderBottom: `1px solid ${theme.colors.secondary.lightGrey}`,
+        },
+
+        "& li:hover": {
+            backgroundColor: '#fff',
+            color: `${theme.colors.secondary.mediumNavy}`,
+        },
+
+        "& .Mui-selected, .Mui-selected:hover": {
+            backgroundColor: '#fff',
+        },
+    },
+});
+
+export default function EncodeType({
+    formControlProps,
+    labelText,
+    id,
+    labelProps,
+    inputProps,
+}) {
+    const classes = useStyles();
+
+    return (
+        <FormControl {...formControlProps}>
+            {labelText !== undefined ? (
+                <StyledSelectInput
+                    htmlFor={id}
+                    {...labelProps}
+                >
+                    {labelText}
+                </StyledSelectInput>
+            ) : null}
+            <StyledSelect style={{ boxShadow: "none" }}
+                id={id}
+                className={classes.root}
+                {...inputProps}
+                MenuProps={{
+                    getContentAnchorEl: null,
+                    anchorOrigin: {
+                        vertical: 'bottom',
+                        horizontal: 'left'
+                    },
+                    classes: { paper: classes.select }
+                }}
+            >
+                <MenuItem style={{ cursor: "pointer" }} value="Music">Music</MenuItem>
+                <MenuItem style={{ cursor: "pointer" }} value="Video">Video</MenuItem>
+                <MenuItem style={{ cursor: "pointer" }} value="Audio">Audio</MenuItem>
+            </StyledSelect>
+        </FormControl>
+    );
+}

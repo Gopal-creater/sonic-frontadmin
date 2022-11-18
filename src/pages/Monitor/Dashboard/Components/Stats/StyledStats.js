89a52d334ee9325c331cdd6963c6c0338650b5b8
Gopal1926 <gopal@arbadev.com>
2022-11-18 12:11:24 +0545

    changed dashboard cards icon box

--- src/pages/Monitor/Dashboard/Components/Stats/StyledStats.js
+++ src/pages/Monitor/Dashboard/Components/Stats/StyledStats.js
@@ -40,7 +40,7 @@ export const CardContainer = styled(Grid)`
   padding: 10px;
   margin-top: -30px;
 
-  background-color: ${(props) => props.theme.background.dark1};
+  background-color: ${(props) => props.theme.colors.primary.main};
   border-radius: 10px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
